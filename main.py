dfcfc81e62e4dcd87426b3013bdb5ec93dbeccfc
alyssa-adams <amadam15@asu.edu>
2020-03-30 18:08:50 -0500

    cleaned up code, needs to be refactored to separate functions and plots

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from pybdm import BDM
 import numpy as np
 import seaborn as sns; sns.set()
 import matplotlib.pyplot as plt
+import statistics
 
 
 # need to set up an initial board
@@ -97,7 +98,7 @@ def make_chess_db():
         df = df.append(df2, ignore_index=True)
 
     # pickle to save the resulting chess db
-    with open('matches_df', 'wb') as handle:
+    with open('pickle_jar/matches_df', 'wb') as handle:
         pickle.dump(df, handle)
 
 
@@ -110,7 +111,7 @@ def make_chess_db():
 def make_states():
 
     # load in the db
-    df = pickle.load(open('matches_df', 'rb'))
+    df = pickle.load(open('pickle_jar/matches_df', 'rb'))
 
     # need to save these to a file one at a time, because it takes too long to hold in memory
 
@@ -166,13 +167,13 @@ def make_states():
 
         # breaks to here. If it's actually a valid move, save states and things to list
         if not bad_move:
-            with open("all_states", "a+") as myfile:
+            with open("pickle_jar/all_states", "a+") as myfile:
                 myfile.write(str(states) + '\n')
-            with open("all_n_moves", "a+") as myfile:
+            with open("pickle_jar/all_n_moves", "a+") as myfile:
                 myfile.write(str(n_possible_moves) + '\n')
 
         else:
-            with open("remove_matches", "a") as myfile:
+            with open("pickle_jar/remove_matches", "a") as myfile:
                 myfile.write(str(k) + '\n')
 
 
@@ -184,11 +185,11 @@ def refine_df():
     # get the bdm values
 
     # load in the db
-    df = pickle.load(open('matches_df', 'rb'))
+    df = pickle.load(open('pickle_jar/matches_df', 'rb'))
 
     # remove the matches with the bad moves from the df
     remove_matches = []
-    with open('remove_matches') as fp:
+    with open('pickle_jar/remove_matches') as fp:
         for line in fp:
             remove = int(re.sub('\n', '', line))
             remove_matches.append(remove)
@@ -196,17 +197,17 @@ def refine_df():
 
     # add one new column for n possible states
     all_n_moves = []
-    with open('all_n_moves') as fp:
+    with open('pickle_jar/all_n_moves') as fp:
         for line in fp:
             line = ast.literal_eval(line)
             all_n_moves.append(line)
     df['n_possible_moves'] = all_n_moves
 
     # drop moves column to save memory a little
-    df = df.drop(columns=['moves'])
+    #df = df.drop(columns=['moves'])
 
     # pickle to save the resulting chess db
-    with open('matches_df_refined', 'wb') as handle:
+    with open('pickle_jar/matches_df_refined', 'wb') as handle:
         pickle.dump(df, handle)
 
 
@@ -219,7 +220,7 @@ def calculate_bdm():
     bdm_one = BDM(ndim=2, nsymbols=2, warn_if_missing_ctm=False)
 
 
-    with open('all_states') as fp:
+    with open('pickle_jar/all_states') as fp:
 
         for line in fp:
 
@@ -248,51 +249,86 @@ def calculate_bdm():
 
 
             # save to file as you chug along. Then this can be used for a plot later
-            with open("bdms_white_all", "a+") as myfile:
+            with open("pickle_jar/bdms_white_all", "a+") as myfile:
                 myfile.write(str(bdms_white) + '\n')
-            with open("bdms_black_all", "a+") as myfile:
+            with open("pickle_jar/bdms_black_all", "a+") as myfile:
                 myfile.write(str(bdms_black) + '\n')
 
 
 #calculate_bdm()
 
 
-def plot_results():
+def plot_bdm_results():
+
+    # make a folder for the figures
+    figure_folder = 'figures'
+    if not os.path.exists(figure_folder):
+        os.makedirs(figure_folder)
 
     # load in the db
-    df = pickle.load(open('matches_df_refined', 'rb'))
+    df = pickle.load(open('pickle_jar/matches_df_refined', 'rb'))
     df = df.reset_index(drop=True)
 
-    xs = []
-    ys = []
+    '''
+    These are the DF columns:
+    
+    data['white_players'] = []
+    data['black_players'] = []
+    data['white_elos'] = []
+    data['black_elos'] = []
+    data['winners'] = []
+    data['moves'] = []
+    data['n_possible_moves'] = []
+    '''
+
+    bdm_pickles = ['bdms_white_all', 'bdms_black_all']
 
     # plot a line
-    with open('bdms_white_all') as fp:
-        for i, line in enumerate(fp):
-            line = re.sub('\n', '', line)
-            line = ast.literal_eval(line)
-            y = sum(line)/len(line)
-            x = df.iloc[[i]]['white_elos']
-            ys.append(y)
-            xs.append(x)
+    # elo vs bdm
+    # n moves vs bdm
 
-    ax = sns.scatterplot(x=xs, y=ys, linewidth=0, s=1, alpha=0.7, legend=False)
-    plt.xlabel('White ELO')
-    plt.ylabel('Mean BDM')
-    plt.show()
+    for bw in bdm_pickles:
 
-    with open('bdms_black_all') as fp:
-        for i, line in enumerate(fp):
-            line = re.sub('\n', '', line)
-            line = ast.literal_eval(line)
-            y = sum(line)/len(line)
-            x = df.iloc[[i]]['black_elos']
-            ys.append(y)
-            xs.append(x)
+        # do this for the mean, media, and mode
+        for m in ['mean', 'median', 'max']:
 
-    ax = sns.scatterplot(x=xs, y=ys, linewidth=0, s=1, alpha=0.7, legend=False)
-    plt.xlabel('Black ELO')
-    plt.ylabel('Mean BDM')
-    plt.show()
+            # plot outfile name
+            player_type = bw.split('_')[1]
+            plot_out = 'moves' + '_' + 'bdm' + '_' + player_type + '_' + m + '.png'
+            plot_out = os.path.join(figure_folder, plot_out)
 
-plot_results()
+            xs = []
+            ys = []
+
+            with open(bw) as fp:
+                for i, line in enumerate(fp):
+
+                    y = re.sub('\n', '', line)
+                    y = ast.literal_eval(y)
+
+                    x = list(df.iloc[[i]]['n_possible_moves'])[0]
+
+                    if m == 'mean':
+                        x = sum(x)/len(x)
+                        y = sum(y)/len(y)
+
+                    elif m == 'median':
+                        x = statistics.median(x)
+                        y = statistics.median(y)
+
+                    elif m == 'max':
+                        x = max(x)
+                        y = max(y)
+
+                    ys.append(y)
+                    xs.append(x)
+
+            plt.plot()
+            sns.scatterplot(x=xs, y=ys, linewidth=0, s=1, alpha=0.7, legend=False)
+            plt.xlabel(m + ' n possible moves')
+            plt.ylabel(m + ' ' + player_type + ' bdm')
+            plt.savefig(plot_out)
+            plt.clf()
+
+
+plot_bdm_results()
